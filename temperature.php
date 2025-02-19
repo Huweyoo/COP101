1599f92e1597f6334a6c4c61bc81c48a2532cba2
Liev03 <uriels.evangelista@gmail.com>
2025-02-20 04:54:42 +0800

    fixed time

--- temperature.php
+++ temperature.php
@@ -192,7 +192,6 @@ function fetchBreakdownData() {
 
             filteredData.forEach(row => {
                 let dateTime = new Date(row.last_saved).toLocaleString('en-US', { 
-                    month: 'short', day: '2-digit', year: 'numeric', 
                     hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true 
                 });
 
